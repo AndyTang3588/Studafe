ae9a328ea3338ad60b9dc4d85e96e454e2020457
FuLu糖福禄 <tanghaochen0506@hotmail.com>
2025-05-01 02:48:11 +0200

    光照、性能优化；镜子修复；防音室参数修改
    新增实时光，二楼的打光添加了灯泡和电线模型。
    对一二楼的实时光/视频播放器元素，使用AreaOnOff进行优化
    Reflection Probe修复
    内-内降低音量，内-外提高音量

---
+++ Studafé/Assets/预制件/AreaOnOff/Script/AreaOnOff.cs
@@ -0,0 +1,45 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class AreaOnOff : UdonSharpBehaviour
+{
+    [Header("Objects to Show")]
+    [Tooltip("The objects you want to show when player touches the cube")]
+    [SerializeField]
+    private GameObject[] objectsToShow;
+
+    private void Start()
+    {
+        // Ensure objects are hidden at start
+        foreach (GameObject obj in objectsToShow)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        // Check if the player is the local player
+        if (player.isLocal)
+        {
+            foreach (GameObject obj in objectsToShow)
+            {
+                obj.SetActive(true);
+            }
+        }
+    }
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        // Check if the player is the local player
+        if (player.isLocal)
+        {
+            foreach (GameObject obj in objectsToShow)
+            {
+                obj.SetActive(false);
+            }
+        }
+    }
+}
