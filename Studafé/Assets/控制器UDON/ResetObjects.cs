5b432e63f9f44b0006e6d4bc9f830f942771b340
FuLu糖福禄 <tanghaochen0506@hotmail.com>
2025-05-17 02:22:04 +0200

    枕头改为固定；默认bgm改为bili源；播放器进度条bug修复

---
+++ Studafé/Assets/控制器UDON/ResetObjects.cs
@@ -0,0 +1,41 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ResetObjects : UdonSharpBehaviour
+{
+    [Header("需要重置的物体")]
+    public GameObject[] objectsToReset;
+
+    private Vector3[] initialPositions;
+    private Quaternion[] initialRotations;
+
+    void Start()
+    {
+        int count = objectsToReset.Length;
+        initialPositions = new Vector3[count];
+        initialRotations = new Quaternion[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            if (objectsToReset[i] != null)
+            {
+                initialPositions[i] = objectsToReset[i].transform.position;
+                initialRotations[i] = objectsToReset[i].transform.rotation;
+            }
+        }
+    }
+
+    public override void Interact()
+    {
+        for (int i = 0; i < objectsToReset.Length; i++)
+        {
+            if (objectsToReset[i] != null)
+            {
+                objectsToReset[i].transform.position = initialPositions[i];
+                objectsToReset[i].transform.rotation = initialRotations[i];
+            }
+        }
+    }
+}
